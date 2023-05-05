9c65bbad298564157174e8fcf0801cd1cd773bc2
coolmark12 <126126803+coolmark12@users.noreply.github.com>
2023-05-05 21:20:59 +0800

    Add files via upload

--- coursedb.sql
+++ coursedb.sql
@@ -2,10 +2,10 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Apr 22, 2023 at 01:55 PM
--- Server version: 10.4.28-MariaDB
--- PHP Version: 8.2.4
+-- 主機： 127.0.0.1
+-- 產生時間： 2023-05-05 15:16:40
+-- 伺服器版本： 10.4.28-MariaDB
+-- PHP 版本： 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,13 +18,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `coursedb`
+-- 資料庫： `coursedb`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `course`
+-- 資料表結構 `course`
 --
 
 CREATE TABLE `course` (
@@ -42,10 +42,15 @@ CREATE TABLE `course` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `course`
+-- 傾印資料表的資料 `course`
 --
 
 INSERT INTO `course` (`course_id`, `course_name`, `course_credit`, `course_type`, `teachers_name`, `max_student`, `current_student`, `time`, `day`, `department_name`, `grade`) VALUES
+(879, '流體力學', 3, '必修', '黃振家', 60, 56, '0900~1200', 3, '土木工程系', 2),
+(895, '鋼結構設計', 3, '必修', '王起平', 60, 48, '0900~1200', 3, '土木工程系', 3),
+(901, '工程測量', 2, '選修', '林建良', 60, 22, '2025~2210', 3, '土木工程系', 3),
+(906, '深開挖分析原理及程式應用', 3, '選修', '陳廣祥', 60, 31, '1830~2115', 3, '土木工程系', 3),
+(908, '量測系統學', 2, '選修', '廖為忠', 60, 17, '1400~1600', 2, '土木工程系', 3),
 (1200, '程式設計（一）', 2, '必修', '劉明機', 10, 7, '0800~1200', 3, '資訊工程系', 1),
 (1260, '系統程式', 3, '必修', '劉宗杰', 10, 7, '1000~1200', 1, '資訊工程系', 2),
 (1261, '資料庫系統', 3, '必修', '許懷中', 10, 7, '1000~1200', 2, '資訊工程系', 2),
@@ -54,12 +59,22 @@ INSERT INTO `course` (`course_id`, `course_name`, `course_credit`, `course_type`
 (1264, '多媒躰系統', 3, '選修', '葉春秀', 10, 7, '1300~1600', 2, '資訊工程系', 2),
 (1265, '組合數學', 3, '選修', '游景盛', 10, 7, '1600~1800', 2, '資訊工程系', 2),
 (1269, 'UNIX應用與實務', 3, '選修', '竇其仁', 10, 7, '1600~1800', 3, '資訊工程系', 2),
-(1334, '計算機概論', 3, '必修', '林峰正', 10, 7, '1600~1800', 4, '資訊工程系', 1);
+(1334, '計算機概論', 3, '必修', '林峰正', 10, 7, '1600~1800', 4, '資訊工程系', 1),
+(2404, '科技人文導論', 2, '必修', '余風', 60, 53, '1000~1200', 1, '中文系', 1),
+(2405, '現代文學史', 2, '必修', '鄭慧如', 70, 49, '1000~1200', 2, '中文系', 1),
+(2408, '看動漫學英文', 2, '選修', '闕帝丰', 70, 59, '0800~1000', 1, '中文系', 1),
+(2419, '新聞採訪與寫作', 2, '選修', '朱文光', 70, 44, '1500~1700', 1, '中文系', 2),
+(2430, '影視劇本實作', 2, '選修', '劉梓潔', 40, 37, '1000~1200', 4, '中文系', 3),
+(2480, '固態物理導論', 3, '選修', '蔡雅芝', 60, 34, '1500~1700', 3, '光電科學與工程學系', 2),
+(2484, '光電半導體元件導論', 3, '必修', '唐謙仁', 62, 62, '1400~1700', 4, '光電科學與工程學系', 3),
+(2485, '雷射與光子學導論', 3, '必修', '林泰生', 60, 57, '1000~1200', 1, '光電科學與工程學系', 3),
+(2494, '數位光學造影技術', 3, '選修', '林昱志', 40, 13, '0900~1200', 4, '光電科學與工程學系', 3),
+(2495, '積體光學', 3, '選修', '羅仕守', 60, 38, '1830~2115', 1, '光電科學與工程學系', 3);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `courseselection`
+-- 資料表結構 `courseselection`
 --
 
 CREATE TABLE `courseselection` (
@@ -68,7 +83,7 @@ CREATE TABLE `courseselection` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `courseselection`
+-- 傾印資料表的資料 `courseselection`
 --
 
 INSERT INTO `courseselection` (`student_id`, `course_id`) VALUES
@@ -111,7 +126,7 @@ INSERT INTO `courseselection` (`student_id`, `course_id`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `student`
+-- 資料表結構 `student`
 --
 
 CREATE TABLE `student` (
@@ -122,7 +137,7 @@ CREATE TABLE `student` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `student`
+-- 傾印資料表的資料 `student`
 --
 
 INSERT INTO `student` (`student_id`, `student_name`, `department_name`, `grade`) VALUES
@@ -133,34 +148,34 @@ INSERT INTO `student` (`student_id`, `student_name`, `department_name`, `grade`)
 ('D1095589', '盧重毓', '資訊工程系', 2);
 
 --
--- Indexes for dumped tables
+-- 已傾印資料表的索引
 --
 
 --
--- Indexes for table `course`
+-- 資料表索引 `course`
 --
 ALTER TABLE `course`
   ADD PRIMARY KEY (`course_id`);
 
 --
--- Indexes for table `courseselection`
+-- 資料表索引 `courseselection`
 --
 ALTER TABLE `courseselection`
   ADD PRIMARY KEY (`student_id`,`course_id`),
   ADD KEY `course_id` (`course_id`);
 
 --
--- Indexes for table `student`
+-- 資料表索引 `student`
 --
 ALTER TABLE `student`
   ADD PRIMARY KEY (`student_id`);
 
 --
--- Constraints for dumped tables
+-- 已傾印資料表的限制式
 --
 
 --
--- Constraints for table `courseselection`
+-- 資料表的限制式 `courseselection`
 --
 ALTER TABLE `courseselection`
   ADD CONSTRAINT `course_id` FOREIGN KEY (`course_id`) REFERENCES `course` (`course_id`) ON DELETE CASCADE ON UPDATE CASCADE,
