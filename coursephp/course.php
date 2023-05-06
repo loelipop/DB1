772553d2f21f2df1201dd0db9f2d3268db8d9e7a
loelipop <96033874+loelipop@users.noreply.github.com>
2023-05-06 22:44:02 +0800

    Update course.php

--- coursephp/course.php
+++ coursephp/course.php
@@ -35,7 +35,8 @@
         $row = $result->fetch_assoc();
         echo "學號：" . $row["student_id"] . "<br>";
         echo "姓名：" . $row["student_name"] . "<br>";
-        echo "系級：" . $row["department_name"] . "<br>";
+        echo "系所：" . $row["department_name"] . "<br>";
+        echo "年級：" . $row["grade"] . "<br>";
 		
 		// 顯示已選課表按鈕
 		echo '<button onclick="location.href=\'?action=show_courses&student_id='.$row["student_id"].'\'">已選課表</button>';
@@ -71,8 +72,8 @@
         // 顯示已選課表資訊和已選總學分數
         echo "<h2>已選課表</h2>";
 		
-		// 新增返回主頁的按鈕
-		echo '<button onclick="window.history.back()">返回主頁</button>';
+		    // 新增返回主頁的按鈕
+		    echo '<button onclick="window.history.back()">返回主頁</button>';
 		
         echo "<table>";
         echo "<tr><th>課程編號</th><th>課程名稱</th><th>時段</th><th>上課日</th><th>學分數</th><th>課程類別</th><th>教師姓名</th></tr>";
@@ -111,24 +112,11 @@
               } else {
                 //進行退選
                 $sql = "DELETE FROM courseselection WHERE student_id='".$_GET["student_id"]."' AND course_id='".$_POST["drop_course_id"]."'";
-                if($conn->query($sql) === TRUE) {
+                if($conn->query($sql) == TRUE && mysqli_affected_rows($conn) > 0) {
                   echo "退選成功";
-                  //取的已選課表資訊
-                  $sql = "SELECT course.course_id,course.course_name,course.time,course.day,course.course_credit,course.course_type,course.teachers_name FROM course INNER JOIN courseselection ON course.course_id = courseselection.course_id WHERE student_id = '".$_GET["student_id"]."'ORDER BY course.day ASC, course.time ASC";
-                  $result = $conn->query($sql);
                   //更新課程人數
                   $sql = "UPDATE course SET current_student = (current_student - 1)";
                   $conn->query($sql);
-                  //顯示退選後的課表
-                  echo "<h2>已選課表</h2>";
-                  echo "<table>";
-                  echo "<tr><th>課程編號</th><th>課程名稱</th><th>時段</th><th>上課日</th><th>學分數</th><th>課程類別</th><th>教師姓名</th></tr>";
-                  mysqli_data_seek($result, 0);
-                  while($row = $result->fetch_assoc()) {
-                    echo "<tr><td style='text-align: center;'>" . $row["course_id"] . "</td><td style='text-align: center;'>" . $row["course_name"] . "</td><td style='text-align: center;'>" . $row["time"] . "</td><td style='text-align: center;'>". $row["day"] . "</td><td style='text-align: center;'>". $row["course_credit"] . "</td><td style='text-align: center;'>". $row["course_type"] . "</td><td style='text-align: center;'>". $row["teachers_name"] . "</td></tr>";
-                  }
-                  echo "</table>";
-                  echo "<p>已選總學分數： " . $total_credit . "</p>";
                 }else{
                   echo "退選失敗" . $conn->error;
                 }
